082d4d1e614e7cebbc38a28cf7f812846c281e66
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-08 16:49:18 +0900

    data 넘기기

---
+++ step3/components/ListHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>리스트 해더</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ step3/components/ListPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="test1">
+        <list-header></list-header>
+        <div>
+            <ul>
+                <li v-for="memo in list">{{memo}}</li>
+            </ul>
+        </div>
+        <list-footer></list-footer>
+    </div>
+</template>
+
+<script>
+import ListFooter from './ListFooter';
+import ListHeader from './ListHeader';
+
+export default {
+    components: {
+        ListHeader,
+        ListFooter
+    },
+    data(){
+        // return { list:["memo1", "memo2", "memo3"]}
+    },
+    props : {
+        list : {
+            type:Array,
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+.test1 {
+    font-size: 20px;
+}
+</style>
