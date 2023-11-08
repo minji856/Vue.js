2ccc36e86487637748bb458d9c96b1299a505234
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-08 16:46:07 +0900

    Vue_cli지역컴퍼넌트

---
+++ step1/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>로컬 컴퍼넌트</h1>
+  <hr>
+
+  <div id="app">
+    <list-page></list-page>
+  </div>
+</template>
+
+<script>
+  /* 지역 컴퍼넌트 사용 */
+  import ListPage from "./components/ListPage";
+  
+  export default {
+    components:{
+      ListPage
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ step1/components/ListPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="test1">
+        <list-header></list-header>
+        <div>리스트 페이지입니다.</div>
+        <list-footer></list-footer>
+    </div>
+</template>
+
+<script>
+import ListFooter from './ListFooter';
+import ListHeader from './ListHeader';
+
+export default {
+    components: {
+        ListHeader,
+        ListFooter
+    }
+}
+</script>
+
+<style>
+.test1 {
+    font-size: 20px;
+}
+</style>
