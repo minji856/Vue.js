0a7b431a66a581d7e2a8c0b15bbfcb7d84136ddc
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-10 12:58:32 +0900

    common.js 생성으로 MODE export

---
+++ src/src/step4/pages/ReadPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <header-component title="읽기 페이지"></header-component>
+    <div>
+        <ul>
+            <p>선택된 메모 = {{currentMemo}}</p>
+        </ul>
+    </div>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script>
+export default {
+    props : {
+        currentMemo : {
+            type: String,
+            default : ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
