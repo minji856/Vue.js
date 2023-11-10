dc193bf30aa9e453196377ca03dd2dd6fc5828f2
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-10 11:03:56 +0900

    지역컴퍼넌트

--- step3/App.vue
+++ step3/App.vue
@@ -1,53 +1,34 @@
 <template>
-    <div>
-        <h1>컴퍼넌트를 이용한 메모장</h1>
-    </div>
-    <div id="app">
-        <!-- 바인딩으로 데이터 던지기 -->
-                <!-- 이렇게 해서 props에 연결할 수 있음 -->
-        <list-page :list="list" :setReadMemo="setReadMemo" v-if="mode == 'list'"></list-page>
-        <read-page :current-memo="selectedMemo" v-if="mode == 'read'"></read-page>
-    </div>
+  <h1>로컬 컴퍼넌트</h1>
+  <hr>
+
+  <div id="app">
+    <list-page :list="list"></list-page>
+  </div>
 </template>
 
 <script>
-    import ListPage from "./pages/ListPage"
-    import ReadPage from "./pages/ReadPage"
-
-    const MODE = {
-        LIST : "list",
-        READ : "read"
+  /* 지역 컴퍼넌트 사용 */
+  import ListPage from "./components/ListPage";
+  
+  export default {
+    components:{
+      ListPage
+    },
+    data (){
+        return { list : ["memo1", "memo2", "memo3", "memo4"]
+      }
     }
-
-    export default {
-        components: {
-            ListPage,
-            ReadPage
-        },
-        data(){
-            return { 
-                list : ["memo1", "memo2", "memo3"],
-                selectedMemo : "",
-                mode : MODE.LIST
-            }
-        },
-        methods : {
-            setReadMemo(index){
-                this.selectedMemo = this.list[index];
-                console.log(this.selectedMemo, index);
-                this.mode = MODE.READ
-            }
-        }
-}
+  }
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
