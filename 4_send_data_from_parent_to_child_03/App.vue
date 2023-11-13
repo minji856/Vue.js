d15eb05f294018cbb0eba2af6e5f1a401b1697b1
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-13 17:10:58 +0900

    v-slot 디렉티브 , inheritAttrs: false 로 속성막기, 등 다양한 속성

---
+++ 4_send_data_from_parent_to_child_03/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- 다양한 속성 -->
+    <!--<my-btn> camel 표기법으로도 가능-->
+    <!-- <MyBtn class="mycss"
+      style="color: red;">
+      MyButton
+    </MyBtn> -->
+
+    <MyBtn>
+      <template v-slot:icon>
+        <span>(A)</span>
+      </template>
+      <template #text>
+        <span>MyButton</span>
+      </template>
+    </MyBtn>
+</template>
+
+<script>
+  import MyBtn from "./components/MyBtn"
+
+  export default {
+    components : {
+      MyBtn
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} 
+</style>
---
+++ 4_send_data_from_parent_to_child_03/components/MyBtn.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="btn">
+    <!-- slot 이름을 지정해주면 원하는 곳에 넣을 수 있다 -->
+    <slot name="icon"></slot>
+    <slot name="text"></slot>
+  </div>
+    <!--
+    <h1 :class="$attrs.class"
+    :style="$attrs.style">test</h1> 
+    -->
+    <!-- <h1 v-bind="$attrs">test</h1> -->
+</template>
+
+<script>
+    export default {
+        // 상속자체를 막아버리는 속성
+        //inheritAttrs: false,
+        created (){
+            console.log(this.$attrs)
+        }
+    }
+    
+</script>
+
+<style>
+    .btn {
+        display: inline-block;
+        margin: 4px;
+        padding: 6px 12px;
+        border-radius: 4px;
+        background-color: gray;
+        color: white;
+        cursor: pointer;
+    }
+</style>
