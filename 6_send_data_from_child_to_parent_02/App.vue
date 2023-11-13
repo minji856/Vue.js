33946580d44a2cb79c4de47259f41a80965e692d
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-13 17:14:27 +0900

    watch프로퍼티와 emits로 양방향 전달

---
+++ 6_send_data_from_child_to_parent_02/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- 실제 click 이벤트가 아니다 -->
+    <my-btn @click="log" @changeMsg="logMsg">
+        MyButton
+    </my-btn>
+</template>
+
+<script>
+  import MyBtn from "./components/MyBtn"
+
+  export default {
+    components : {
+      MyBtn
+    },
+    methods : {
+      log(ev){
+        console.log('Click!');
+        console.log(ev);
+      },
+      logMsg(msg){
+        console.log(msg)
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} 
+</style>
---
+++ 6_send_data_from_child_to_parent_02/components/MyBtn.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="btn">
+        <slot></slot>
+    </div>
+    <h1 @dblclick="$emit('click', $event)">test</h1>
+    <!-- 부모에게 입력 받은 값 전달한다 -->
+    <input type="text" v-model="msg"/>
+</template>
+
+<script>
+    export default {
+        // inheritAttrs : false 눌러도 click 콘솔이 안 뜸
+        // 부모에게 전달할 때 넘겨주는 이름
+        emits : ["click", "changeMsg"],
+        data(){
+            return { msg : "" }
+        },
+        // 모니터링 해주는 프로퍼티
+        watch : {
+            msg(){
+                this.$emit('changeMsg', this.msg)
+            }
+        }
+    }
+</script>
+
+<style>
+    .btn {
+        display: inline-block;
+        margin: 4px;
+        padding: 6px 12px;
+        border-radius: 4px;
+        background-color: gray;
+        color: white;
+        cursor: pointer;
+    }
+</style>
