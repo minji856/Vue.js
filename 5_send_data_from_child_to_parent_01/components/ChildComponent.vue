2bca5b09b603adc46152bcd10af68bb23f60c861
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-13 17:12:28 +0900

    자식에게서 부모로 데이터 보내기. 자식을 변수(const)로 따로 이름 지정

---
+++ 5_send_data_from_child_to_parent_01/components/ChildComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="child">
+        Child 컴포넌트
+        <div>
+            <ul>
+                <li v-for="(memo, index) in list" :key="index"
+                @click="onSelected(memo)">{{index}}.{{ memo }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    const ChildComponent = {
+        name: "child",
+        data() { return {
+            list : ["memo1", "memo2", "memo3"]
+            } 
+        },
+        methods : {
+            onSelected(memo){
+                // 부모에게 무엇을 전달할 건지 포장해서 보내주기(선택한 내용)
+                this.$emit(ChildComponent.SELECT, memo);
+            }
+        }
+    }   
+    // 임의로 SELECT라고 한거고 아무 의미는 없다
+    ChildComponent.SELECT = "select";
+
+
+    // 여러개가 만들어질 수 있으니까 아래처럼 자식은 이름으로 구별
+    export default ChildComponent;
+</script>
+
+<style scoped>
+.child {
+    border: 1px solid #000000;
+    margin: 10px;
+}
+</style>
---
+++ 5_send_data_from_child_to_parent_01/components/ParentComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="parent">
+        Parent 컴포넌트
+        <div>
+            자식에게 받은 데이터 = {{ selectedMemo }}
+        </div>
+        <!-- 실제 이벤트가 발생하는 곳으로 연결만 하는 기능. 클릭 X -->
+        <child-component @select="onSelectedMemo"></child-component>
+    </div>
+</template>
+
+<script>
+import ChildComponent from "./ChildComponent";
+
+export default {
+    name: "parent",
+    data() { 
+        return { 
+            selectedMemo : ""
+        } 
+    },
+    components: { ChildComponent },
+    methods : {
+        onSelectedMemo(memo){
+            this.selectedMemo = memo;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.parent {
+    border: 1px solid #000000;
+    margin: 10px;
+}
+</style>
